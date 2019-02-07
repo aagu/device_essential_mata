754a12c30ec56c7cc99257bcbd90245be7cfbb29
aagu <aaguisme@gmail.com>
2019-02-07 18:32:27 +0800

    prebuilt GCam

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
-    Snap
+    GCam
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
